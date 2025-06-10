abd8cabdf94392c69b8e57d344aacb381f44dbc3
martapla <martaplanella@hotmail.com>
2025-06-10 15:58:58 +0200

    wip step-1

---
+++ frontend/src/components/CheckList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CheckList = () => {
+  return (
+    <div className='checklist-main'>
+        <p>✅  Acces to 100+ GAMES for FREE thanks to ads</p>
+        <p>✅  Log In Across All Yous Devices </p>
+        <p>✅  Skip the Line with Cutomer Support</p>
+    </div>
+  )
+}
+
+export default CheckList
--- frontend/src/components/Step1Connect.jsx
+++ frontend/src/components/Step1Connect.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import CheckList from './CheckList'
 
 const Step1Connect = () => {
   return (
     <div className='step1-main'>
-        <h1>Step1Connect - Connect your Account</h1>
-        <p>...and unlock your benefits!</p>
+
+       
+        <div>
+            <h1> Connect your Account</h1>
+            <p>...and unlock your benefits!</p>
+        </div>
+
+       < CheckList />
+
+       <div>
+
+            <input type="email" 
+                placeholder='Email Address'
+                className='input-email'
+                required
+            />
+            <p> Send Me Offers, News, and Fun Stuff!</p>
+
+            <button onClick={() => alert('Connect your account')}
+                    className='btn-connect'>
+                    Connect 
+
+            </button>
+       </div>
+
 
 
     </div>
